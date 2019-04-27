62a9556d7b9c3ce1d064682117e5e75466fd210a
islamsalah2020 <islamsalah830@gmail.com>
2019-04-27 15:14:26 +0200

    list all projects for Specific user

--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render , HttpResponse
-from .models import Category
+from django.shortcuts import render, HttpResponse
+from .models import Category, CustomUser, Project
 from project.forms import ProjectCreationForm, CatCreationForm
 
 
@@ -30,3 +30,9 @@ def create_cat(request):
         args = {'form': form}
         return render(request, 'project/CreateCat.html', args)
 
+
+def myprojects(request, uid):
+    # if request.method == 'GET':
+    #     user = Project.objects.get(creator=uid)
+    user = CustomUser.objects.get(id=uid)
+    return render(request, 'project/list_all.html', {"user": user})
