66f66084e0b42343bcb7cb3fce923108be8638f5
hagar <hagar@localhost.localdomain>
2019-04-29 11:40:14 +0200

    log in style

--- hatFloos/settings.py
+++ hatFloos/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',  # new hager
+    'crispy_forms',  # new hager
 ]
 AUTH_USER_MODEL = 'user.CustomUser'  # new hager
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -132,4 +134,3 @@ LOGIN_REDIRECT_URL = '/users/'
 LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
