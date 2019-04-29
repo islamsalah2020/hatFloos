92d72fb971f4d7fafe71c40e0727d88502a499be
islamsalah2020 <islamsalah830@gmail.com>
2019-04-30 00:36:44 +0200

    adding comment feature to project page

--- project/views.py
+++ project/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render, HttpResponse
 from .models import Category, CustomUser, Project, Donation
+from comment.models import Comment
+from comment.forms import AddCommentForm
 from django.db.models import Sum
 from project.forms import ProjectCreationForm, CatCreationForm
 from django.contrib.auth.decorators import login_required
@@ -7,15 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/users/login')  # redirect when user is not logged in
 def create(request, uid):
-    
     if request.method == 'POST':
-
-        # request.POST.creator_id = uid
-        # print("------------------------------------------------------")
-        # print(request.POST.creator_id)
-        # print("------------------------------------------------------")
-        # print(request.POST)
-        # print("------------------------------------------------------")
 
         form = ProjectCreationForm(request.POST)
         if form.is_valid():
@@ -61,4 +55,24 @@ def myprojects(request, uid):
 
 def project_details(request, pid):
     item = Project.objects.get(id=pid)
-    return render(request, 'project/project_details.html', {"item": item})
+    # here hard coded for current user till add it from url
+    user = CustomUser.objects.get(id=8)
+    comments = item.comment_set.all()
+    # comments = Comment.objects.all()
+    if request.method == "POST":
+        commentform = AddCommentForm(request.POST)
+        edit = commentform.save(commit=False)
+        edit.project = item
+        edit.commenter = user
+        # comment.content = form.cleaned_data['comment_area']
+        edit.save()
+        form = AddCommentForm()
+        return render(request, 'project/project_details.html', {"item": item, "form": form, "comments": comments})
+        # form = AddCommentForm()
+
+        # return render(request, 'project/project_details.html', {"item": item, "form": form})
+    else:
+        # commentform = AddCommentForm()
+        # return render(request, 'project/project_details.html', {"item": item, "form": commentform})
+        form = AddCommentForm
+        return render(request, 'project/project_details.html', {"item": item, "form": form, "comments": comments})
